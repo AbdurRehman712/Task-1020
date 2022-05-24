430cfd7015275b904d2a9ff9ea13a7294da7c49f
Abdur Rehman Majeed <abdur.rehman@kartoa.com>
2022-05-24 10:05:49 +0500

    Initial Commit

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+function darkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+function menu(x) {
+    x.classList.toggle("change");
+    var navmenu = document.body;
+    navmenu.classList.toggle("menu-active");
+}
